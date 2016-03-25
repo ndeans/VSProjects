a2ed7bac17a2ff3dc2765e77bb0d352113822009
ndeans <ndeans@yahoo.com>
2016-03-25 01:58:09 -0700

    Initial commit for messaging test

---
+++ MSMQTest/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+using System.Text;
+using System.Windows.Forms;
+using System.Messaging;
+using static MSMQTest.Program;
+
+namespace MSMQTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Payment myPayment;
+
+            myPayment.Payor = textBox1.Text;
+            myPayment.Payee = textBox2.Text;
+            int i = Convert.ToInt32(textBox3.Text);
+            myPayment.Amount = i;
+            myPayment.DueDate = textBox4.Text;
+
+            System.Messaging.Message msg = new System.Messaging.Message();
+            msg.Body = myPayment;
+
+            MessageQueue msgQ = new MessageQueue(".\\private$\\testqueue");
+            msgQ.Send(textBox1.Text);
+
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            MessageQueue msgQ = new MessageQueue(".\\private$\\testqueue");
+
+            Payment myPayment = new Payment();
+            Object o = new object();
+            System.Type[] arrTypes = new System.Type[2];
+            arrTypes[0] = myPayment.GetType();
+            arrTypes[1] = o.GetType();
+            msgQ.Formatter = new XmlMessageFormatter(arrTypes);
+            myPayment = ((Payment)msgQ.Receive().Body);
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Payment paid to: " + myPayment.Payor);
+            sb.Append("\n");
+            sb.Append("Paid by: " + myPayment.Payee);
+            sb.Append("\n");
+            sb.Append("Amount: $" + myPayment.Amount.ToString());
+            sb.Append("\n");
+            sb.Append("Due Date: " + Convert.ToDateTime(myPayment.DueDate));
+
+            MessageBox.Show(sb.ToString(), "Message Received!");
+            
+        }
+
+    }
+}
---
+++ MSMQTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MSMQTest
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+        public struct Payment
+        {
+            public string Payor, Payee;
+            public int Amount;
+            public string DueDate;
+        }
+
+    }
+}
